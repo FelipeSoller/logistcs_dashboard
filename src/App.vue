1a833029d5d33c7c42b7894da6ea52f9ead0b037
Felipe Soller <soller@outlook.com>
2021-05-01 13:38:44 -0300

    feat: Desenvolve conexão com API utilizando Axios

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,10 @@ export default {
   name: 'App',
   components: {
     
+  },
+  async created() {
+    const { data } = await axios.get('https://6050b01f5346090017670430.mockapi.io/api/routes');
+    console.log(data);
   }
 }
 </script>
