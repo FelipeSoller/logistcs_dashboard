b4c36304e3d41847f49a6145d9938b2a49e18274
Felipe Soller <soller@outlook.com>
2021-05-01 21:44:46 -0300

    feat: Adiciona gráfico de barras e linhas com distância percorrida e média por dia

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="container">
+    <div class="row mt-5" v-if="mergedArray.length > 0">
+      <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12"  align="center">
+        <h2 class="mb-5">DASHBOARD DE LOGÍSTICA</h2>
+        <line-chart :chartData="mergedArray" :options="chartOptions" label="merged"></line-chart>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
+import LineChart from './components/LineChart'
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    LineChart,
+  },
   data() {
     return {
-      totalKmTravelled: [],
+      totalKmTraveled: [],
       totalFines: [],
       mergedArray: [],
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
     };
   },
   async created() {
@@ -20,12 +35,13 @@ export default {
       "https://6050b01f5346090017670430.mockapi.io/api/routes"
     );
 
-    this.totalKmTravelled = Object.values(
+    this.totalKmTraveled = Object.values(
       data.reduce((a, { date, initialKm, finalKm }) => {
-        a[date] = a[date] || { date, initialKm: 0, finalKm: 0, travelledKm: 0 };
+        a[date] = a[date] || { date, initialKm: 0, finalKm: 0, traveledKm: 0, averageTraveld: 0 };
         a[date].finalKm = Number(a[date].finalKm) + Number(finalKm);
         a[date].initialKm = Number(a[date].initialKm) + Number(initialKm);
-        a[date].travelledKm = a[date].finalKm - a[date].initialKm;
+        a[date].traveledKm = a[date].finalKm - a[date].initialKm;
+        a[date].averageTraveld = (a[date].finalKm - a[date].initialKm)/2;
         return a;
       }, {})
     );
@@ -40,17 +56,25 @@ export default {
       }, {})
     );
 
-    function mergeArrayObjects(totalKmTravelled, totalFines) {
-      return totalKmTravelled.map((item, i) => {
+    function mergeArrayObjects(totalKmTraveled, totalFines) {
+      return totalKmTraveled.map((item, i) => {
         if (item.id === totalFines[i].id) {
           return Object.assign({}, item, totalFines[i]);
         }
       });
     }
 
-    this.mergedArray = mergeArrayObjects(this.totalKmTravelled, this.totalFines);
+    this.mergedArray = mergeArrayObjects(this.totalKmTraveled, this.totalFines);
+    console.log(this.mergedArray);
   },
 };
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Antonio&display=swap');
+
+h2 {
+  font-family: 'Antonio', sans-serif;
+}
+
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,76 @@
+<script>
+import { Bar, Line } from "vue-chartjs";
+
+export default {
+  extends: Bar, Line,
+  props: {
+    label: {
+      type: String,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+  },
+  mounted() {
+    const date = this.chartData.map((d) => d.date);
+    const traveledKm = this.chartData.map((d) => d.traveledKm);
+    const averageTraveld = this.chartData.map((d) => d.averageTraveld);
+
+    this.renderChart(
+      {
+        labels: date,
+        datasets: [
+          {
+            label: "Quilômetros rodados totais por dia",
+            data: traveledKm,
+            backgroundColor: [
+              "rgba(255, 0, 0, 0.2)",
+              "rgba(255, 128, 0, 0.2)",
+              "rgba(255, 255, 0, 0.2)",
+              "rgba(250, 7, 61, 0.2)",
+              "rgba(128, 255, 0, 0.2)",
+              "rgba(0, 255, 0, 0.2)",
+              "rgba(0, 255, 128, 0.2)",
+              "rgba(0, 255, 255, 0.2)",
+              "rgba(0, 128, 255, 0.2)",
+              "rgba(0, 0, 255, 0.2)",
+              "rgba(128, 0, 255, 0.2)",
+              "rgba(255, 0, 255, 0.2)",
+              "rgba(255, 0, 128, 0.2)",
+              "rgba(255, 0, 64, 0.2)",
+              "rgba(255, 0, 0, 0.2)",
+            ],
+            borderColor: [
+              "rgb(255, 0, 0)",
+              "rgb(255, 128, 0)",
+              "rgb(255, 255, 0)",
+              "rgb(250, 7, 61)",
+              "rgb(128, 255, 0)",
+              "rgb(0, 255, 0)",
+              "rgb(0, 255, 128)",
+              "rgb(0, 255, 255)",
+              "rgb(0, 128, 255)",
+              "rgb(0, 0, 255)",
+              "rgb(128, 0, 255)",
+              "rgb(255, 0, 255)",
+              "rgb(255, 0, 128)",
+              "rgb(255, 0, 64)",
+              "rgb(255, 0, 0)",
+            ],
+            barThickness: 40
+          },
+          {
+            label: "Média de quilômetros rodados totais no dia",
+            type: "line",
+            data: averageTraveld,
+          }
+        ],
+      },
+      this.options
+    );
+  },
+};
+</script>
